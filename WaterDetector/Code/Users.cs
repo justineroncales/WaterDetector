68c3a92ef1ee83d20342e9629dfb26eca4708db4
justine roncales <justine roncales@DESKTOP-UPJ712P>
2022-05-15 02:15:54 +0800

    Add project files.

---
+++ WaterDetector/Code/Users.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WaterDetector.Code
+{
+    
+    public class Users
+    {
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["WDetector"].ConnectionString);
+        public List<UsersDetails> Login()
+        {
+            List<UsersDetails> usersDetails = new List<UsersDetails>();
+            using (conn)
+            {
+                conn.Open();
+                string query = "select * from dbo.USERS";
+
+                SqlCommand sqlCommand = new SqlCommand(query, conn);
+                sqlCommand.CommandType = System.Data.CommandType.Text;
+                DataTable dt = new DataTable();
+                SqlDataAdapter dataAdapter = new SqlDataAdapter();
+                sqlCommand.CommandTimeout = 0;
+                dataAdapter.SelectCommand = sqlCommand;
+                dataAdapter.Fill(dt);
+
+                foreach (DataRow row in dt.Rows)
+                {
+                    UsersDetails item = new UsersDetails();
+                    item.USERNAME = row["USERNAME"].ToString();
+                    item.PASSWORD = row["PASSWORD"].ToString();
+                    usersDetails.Add(item);
+                }
+               
+            }
+            return usersDetails;
+        }
+    }
+    public class UsersDetails
+    {
+        public string USERNAME { get; set; }
+        public string PASSWORD { get; set; }
+    }
+}
---
+++ WaterDetector/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WaterDetector.Code;
+
+namespace WaterDetector.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Users us = new Users();
+            us.Login();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [HttpGet]
+        public JsonResult GetUsersDetails()
+        {
+            Users us = new Users();
+            List<UsersDetails> usersDetails = new List<UsersDetails>();
+            usersDetails = us.Login();
+            return Json(usersDetails, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
